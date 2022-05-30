8d9b514c1f6759d3bf36c3e63f31b7147f1b3905
Yeetnade <93275338+Yeetnade@users.noreply.github.com>
2022-05-29 23:41:03 -0700

    Made some changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,18 +14,43 @@ public class GameManager : MonoBehaviour
     public Text playerScoreText1;
     public Text playerScoreText2;
 
+    [SerializeField]
+    private GameObject gameOverUI1;
+    [SerializeField]
+    private GameObject gameOverUI2;
+
     public void PlayerScores1()
     {
         _playerScore1++;
         this.playerScoreText1.text = _playerScore1.ToString();
-        ResetRound();
+        if (_playerScore1 == 11)
+        {
+            this.ball.ResetPosition();
+            gameOverUI2.SetActive(true);
+
+        }
+        else
+        {
+            ResetRound();
+        }
+        
     }
 
     public void PlayerScores2()
     {
         _playerScore2++;
         this.playerScoreText2.text = _playerScore2.ToString();
-        ResetRound();
+        if (_playerScore2 == 11)
+        {
+            this.ball.ResetPosition();
+            gameOverUI1.SetActive(true);
+
+        }
+        else
+        {
+            ResetRound();
+        }
+        
     }
 
     private void ResetRound()
